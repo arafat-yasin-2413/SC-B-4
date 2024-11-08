b7e9a5a3690036e40f6094eb5ce1a80e1b28dea4
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-11-09 01:24:51 +0600

    third commit

--- Live Class 2 Nov 8, 2024/g.cpp
+++ Live Class 2 Nov 8, 2024/g.cpp
@@ -2,14 +2,44 @@
 using namespace std;
 #define nl "\n"
 
-// 
+//
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    // int n;
+    // cin >> n;
+
+    // int ar[n];
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> ar[i];
+    // }
+
+    // int sum =0 ;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // cout << ar[i] << " ";
+    //     if (ar[i] % 2 == 0)
+    //     {
+    //         sum = sum + ar[i];
+    //     }
+    // }
+    // cout << sum << endl;
+
+    // int a, b;
+    // cin >> a >> b;
+
+    // int sum = a+b;
+    // cout << sum << endl;
+
+    for (int i = 1;  ; i++)
+    {
+        cout << "world" << nl;
+    }
 
     return 0;
 }
