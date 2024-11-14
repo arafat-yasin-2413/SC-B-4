965761d6d0eeaded8ab9833fbbb083674e6b13c4
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2024-11-14 16:50:26 +0600

    all updated

--- Live Class 3 Nov 12, 2024/PLAYING_WITH_BITS.cpp
+++ Live Class 3 Nov 12, 2024/PLAYING_WITH_BITS.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 #define nl "\n"
@@ -21,7 +22,7 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     int t;
     cin >> t;
     while (t--)
@@ -52,6 +53,76 @@ int main()
         if (n < 1)
         {
             cout << -1 << nl;
+        }
+    }
+    return 0;
+}
+
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define ll long long int
+//
+
+ll power_calculate(ll p)
+{
+    ll total = 1;
+    for (int i = 1; i <= p; i++)
+    {
+        total = 2 * total;
+        // cout<<total<<endl;
+    }
+
+    return total;
+}
+
+// i = 1,
+// total = 1,
+// 2*1 = 2
+// i = 2,
+// total =
+
+// 2,1 - 2
+// 2,2 - 4
+// 2,3 - 8
+// 2,4 - 16
+// 2,5 - 32
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n, k;
+        cin >> n >> k;
+
+        // cout<<power_calculate(4)<<nl;
+        if (k < 1)
+            cout << -1 << nl;
+
+        else
+        {
+            k--;
+            ll ans = 1;
+            for (int i = 30; i >= 1 && k > 0; i--)
+            {
+                ll v = power_calculate(i);
+                if (v + ans < n)
+                {
+                    ans += v;
+                    k--;
+                }
+            }
+
+            cout << ans << nl;
         }
     }
     return 0;
